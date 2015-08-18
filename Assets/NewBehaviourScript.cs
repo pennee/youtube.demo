05815acb92ce9acddf6a396a1c469b1cf31aac2b
pennee <tanpennee9307@gmail.com>
2015-08-18 21:26:24 +0800

    add debuglog

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("Test");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	
+	}
+}
